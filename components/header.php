dc638294d1a20a706169ea0f6e5be0b11672fb49
Lucas Gabriel Kuligowski <lucaskuligowski@gmail.com>
2024-11-25 20:43:49 -0300

    primeiro commit

---
+++ components/header.php
@@ -0,0 +1,4 @@
+<div class="bg-blue-500 text-white p-4">
+    <h1 class="text-xl font-bold">Hello, Tailwind CSS!</h1>
+    <p>Este é um teste com Tailwind no seu projeto PHP.</p>
+</div>
---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+$page = isset($_GET['page']) ? $_GET['page'] : 'home';
+
+$pagePath = "templates/$page.php";
+if (!file_exists($pagePath)) {
+    $page = '404';
+    $pagePath = "templates/404.php";
+}
+?>
+<html>
+    <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PUCStore</title>
+    <link rel="stylesheet" href="assets/css/output.css">
+    </head>
+<header>
+    <?php include 'components/header.php'; ?>
+</header>
+
+<main id="main-content">
+    <?php include $pagePath; ?>
+</main>
+
+<footer>
+    <?php include 'components/footer.php'; ?>
+</footer>
+
+</html>
