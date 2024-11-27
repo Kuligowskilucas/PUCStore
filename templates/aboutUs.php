861f97af7e8703610dbd9f375005b287395d2f81
Lucas Gabriel Kuligowski <lucaskuligowski@gmail.com>
2024-11-26 22:47:54 -0300

    alterações na pagina quem somos

---
+++ templates/aboutUs.php
@@ -0,0 +1,39 @@
+<section class="py-4">
+    <div class="max-w-[1200px] mx-auto px-4 flex items-center justify-between flex-col">
+        <div class="text-center">
+            <p class="font-bold text-2xl mb-5">Quem Somos</p>
+            <p class="text-lg">Na "I Puc", nossa missão é oferecer uma experiência premium com produtos Apple autênticos e suporte especializado para quem busca o melhor em tecnologia. Como revendedores autorizados, nossa prioridade é garantir que nossos clientes tenham acesso a dispositivos de ponta com total segurança e confiança. <br><br> Fundada com o propósito de levar inovação e design de alta qualidade aos nossos clientes, a "I Puc" é movida pela paixão por tecnologia e pelo compromisso em oferecer um atendimento personalizado. Nossa equipe é formada por especialistas em produtos Apple, sempre prontos para orientar e ajudar na escolha ideal.</p>
+        </div>
+        <picture class="rounded-3xl overflow-hidden my-16">
+            <img src="assets/images/store-front.webp" alt="Imagem da loja">
+        </picture>
+        <div class="text-center">
+            <p class="font-bold text-2xl mb-5">Nossa História</p>
+            <p class="text-lg">A "I Puc" nasceu da ideia de trazer uma experiência completa aos usuários de produtos Apple no Brasil. Desde nosso início, buscamos preencher a lacuna entre as inovações globais da Apple e as necessidades locais dos nossos clientes. Com uma trajetória pautada na qualidade e confiabilidade, crescemos para ser reconhecidos como uma das revendedoras mais confiáveis do setor.<br><br>Hoje, a I Puc conta com uma ampla base de clientes satisfeitos e leais, construindo relações duradouras através do nosso compromisso com a excelência. Seja para uso pessoal ou profissional, acreditamos que cada produto Apple tem o poder de transformar a vida das pessoas e nossa missão é tornar essa transformação acessível a todos.</p>
+        </div>
+    </div>
+</section>
+<section class="bg-gray-200 py-4">
+    <div class="max-w-[1200px] mx-auto px-4 flex items-center justify-between flex-col">
+        <div class="flex flex-col items-center w-full">
+            <p class="font-bold text-2xl mb-5">Nosso Compromisso</p>
+            <div class="flex flex-row justify-between w-full">
+                <div class="bg-white rounded-2xl w-80 h-72">
+                    <picture>
+                        <img src="assets/icons/authenticity-icon.webp" alt="Ícone da autenticidade">
+                    </picture>
+                </div>
+                <div class="bg-white rounded-2xl w-80 h-72">
+                    <picture>
+                        <img src="assets/icons/quality-icon.webp" alt="Ícone da autenticidade">
+                    </picture>
+                </div>
+                <div class="bg-white rounded-2xl w-80 h-72">
+                    <picture>
+                        <img src="assets/icons/innovation-icon.webp" alt="Ícone da autenticidade">
+                    </picture>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
