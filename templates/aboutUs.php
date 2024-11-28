3551febd5c673282f13ec8f31817e152a5959078
Lucas Gabriel Kuligowski <lucaskuligowski@gmail.com>
2024-11-27 21:30:37 -0300

    finalizando a pagina sobre nos

--- templates/aboutUs.php
+++ templates/aboutUs.php
@@ -1,14 +1,14 @@
-<section class="py-4">
+<section class="py-8">
     <div class="max-w-[1200px] mx-auto px-4 flex items-center justify-between flex-col">
         <div class="text-center">
-            <p class="font-bold text-2xl mb-5">Quem Somos</p>
+            <p class="font-bold text-2xl mb-8">Quem Somos</p>
             <p class="text-lg">Na "I Puc", nossa missão é oferecer uma experiência premium com produtos Apple autênticos e suporte especializado para quem busca o melhor em tecnologia. Como revendedores autorizados, nossa prioridade é garantir que nossos clientes tenham acesso a dispositivos de ponta com total segurança e confiança. <br><br> Fundada com o propósito de levar inovação e design de alta qualidade aos nossos clientes, a "I Puc" é movida pela paixão por tecnologia e pelo compromisso em oferecer um atendimento personalizado. Nossa equipe é formada por especialistas em produtos Apple, sempre prontos para orientar e ajudar na escolha ideal.</p>
         </div>
         <picture class="rounded-3xl overflow-hidden my-16">
             <img src="assets/images/store-front.webp" alt="Imagem da loja">
         </picture>
-        <div class="text-center">
-            <p class="font-bold text-2xl mb-5">Nossa História</p>
+        <div class="text-center mb-16">
+            <p class="font-bold text-2xl mb-8">Nossa História</p>
             <p class="text-lg">A "I Puc" nasceu da ideia de trazer uma experiência completa aos usuários de produtos Apple no Brasil. Desde nosso início, buscamos preencher a lacuna entre as inovações globais da Apple e as necessidades locais dos nossos clientes. Com uma trajetória pautada na qualidade e confiabilidade, crescemos para ser reconhecidos como uma das revendedoras mais confiáveis do setor.<br><br>Hoje, a I Puc conta com uma ampla base de clientes satisfeitos e leais, construindo relações duradouras através do nosso compromisso com a excelência. Seja para uso pessoal ou profissional, acreditamos que cada produto Apple tem o poder de transformar a vida das pessoas e nossa missão é tornar essa transformação acessível a todos.</p>
         </div>
     </div>
@@ -16,24 +16,63 @@
 <section class="bg-gray-200 py-4">
     <div class="max-w-[1200px] mx-auto px-4 flex items-center justify-between flex-col">
         <div class="flex flex-col items-center w-full">
-            <p class="font-bold text-2xl mb-5">Nosso Compromisso</p>
+            <p class="font-bold text-2xl mb-8">Nosso Compromisso</p>
             <div class="flex flex-row justify-between w-full">
-                <div class="bg-white rounded-2xl w-80 h-72">
+                <div class="flex flex-col justify-between items-center bg-white rounded-2xl w-80 h-72 py-4">
                     <picture>
-                        <img src="assets/icons/authenticity-icon.webp" alt="Ícone da autenticidade">
+                        <img class="w-28" src="assets/icons/authenticity-icon.webp" alt="Ícone da autenticidade">
                     </picture>
+                    <p class="text-sky-400 font-bold text-2xl">Autenticidade</p>
+                    <p class="text-base mx-4 text-justify">Trabalhamos exclusivamente com produtos originais Apple, garantindo que cada cliente tenha uma experiência segura e confiável.</p>
                 </div>
-                <div class="bg-white rounded-2xl w-80 h-72">
+                <div class="flex flex-col items-center justify-between bg-white rounded-2xl w-80 h-72 py-4">
                     <picture>
-                        <img src="assets/icons/quality-icon.webp" alt="Ícone da autenticidade">
+                        <img class="w-28" src="assets/icons/quality-icon.webp" alt="Ícone da autenticidade">
                     </picture>
+                    <p class="text-sky-400 font-bold text-2xl">Qualidade</p>
+                    <p class="text-base mx-4 text-justify">Nossa prioridade é oferecer dispositivos Apple com assistência técnica de alto padrão, proporcionando o melhor suporte possível.</p>
                 </div>
-                <div class="bg-white rounded-2xl w-80 h-72">
+                <div class="flex flex-col items-center justify-between bg-white rounded-2xl w-80 h-72 py-4">
                     <picture>
-                        <img src="assets/icons/innovation-icon.webp" alt="Ícone da autenticidade">
+                        <img class="w-28" src="assets/icons/innovation-icon.webp" alt="Ícone da autenticidade">
                     </picture>
+                    <p class="text-sky-400 font-bold text-2xl">Inovação</p>
+                    <p class="text-base mx-4 text-justify">Mantemos nosso portfólio atualizado com os lançamentos mais recentes da Apple, trazendo o futuro da tecnologia para você.</p>
                 </div>
             </div>
         </div>
+    </div>
+    <div class="max-w-[1200px] mx-auto px-4 flex items-center justify-between flex-col pb-8">
+        <div class="flex flex-col items-center w-full mt-24">
+            <p class="font-bold text-2xl mb-8">Nossa Equipe</p>
+            <div class="flex flex-row justify-between w-full">
+                <div class="flex flex-col justify-between items-center bg-white rounded-2xl w-80 h-56 py-4">
+                    <picture class="max-h-24 overflow-hidden flex items-center justify-center rounded-full">
+                        <img class="w-24 mt-4" src="assets/images/joao-meneses.webp" alt="Ícone da autenticidade">
+                    </picture>
+                    <p class="text-sky-400 font-bold text-2xl">João Meneses</p>
+                    <p class="text-base mx-4 mb-4 text-justify">Desenvolvedor de Software</p>
+                </div>
+                <div class="flex flex-col justify-between items-center bg-white rounded-2xl w-80 h-56 py-4">
+                    <picture class="max-h-24 overflow-hidden flex items-center justify-center rounded-full">
+                        <img class="w-24 mt-4" src="assets/images/prisca-souza.webp" alt="Ícone da autenticidade">
+                    </picture>
+                    <p class="text-sky-400 font-bold text-2xl">Prisca Souza</p>
+                    <p class="text-base mx-4 mb-4 text-justify">Gerente de Vendas</p>
+                </div>
+                <div class="flex flex-col justify-between items-center bg-white rounded-2xl w-80 h-56 py-4">
+                    <picture class="max-h-24 overflow-hidden flex items-center justify-center rounded-full">
+                        <img class="w-24 mt-4" src="assets/images/lucas-kuligowski.webp" alt="Ícone da autenticidade">
+                    </picture>
+                    <p class="text-sky-400 font-bold text-2xl">Lucas Kuligowski</p>
+                    <p class="text-base mx-4 mb-4 text-justify">Especialista Apple</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="bg-sky-950">
+    <div class="max-w-[1200px] mx-auto px-4 flex items-center justify-between flex-col">
+
     </div>
 </section>
